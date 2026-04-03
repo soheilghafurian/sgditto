14b5aab3f8a634b856a9780525b967834dc62942
Soheil Ghafurian <sghaf@meta.com>
2026-04-03 11:36:03 -0400

    Make formula head-only and add CI workflows

--- Formula/sgditto.rb
+++ Formula/sgditto.rb
@@ -2,8 +2,6 @@ class Sgditto < Formula
   desc "Suppress repeated prefixes in consecutive lines"
   homepage "https://github.com/soheilghafurian/sgditto"
   head "https://github.com/soheilghafurian/sgditto.git", branch: "main"
-  url "https://github.com/soheilghafurian/sgditto/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "" # Update after first tagged release
   license "MIT"
 
   def install
